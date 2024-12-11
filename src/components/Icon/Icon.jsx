1462ae96e083a3614ef302480444757f4ebf1922
Joshua Comeau <joshwcomeau@gmail.com>
2024-12-11 16:46:20 -0500

    Start converting from create-react-app

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import {
+  Search,
+  Menu,
+  ShoppingBag,
+  ChevronDown,
+  X,
+} from 'react-feather';
+
+const icons = {
+  search: Search,
+  menu: Menu,
+  'shopping-bag': ShoppingBag,
+  'chevron-down': ChevronDown,
+  close: X,
+};
+
+const Icon = ({ id, color, size, strokeWidth, ...delegated }) => {
+  const Component = icons[id];
+
+  if (!Component) {
+    throw new Error(`No icon found for ID: ${id}`);
+  }
+
+  return (
+    <Wrapper strokeWidth={strokeWidth} {...delegated}>
+      <Component color={color} size={size} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  & > svg {
+    display: block;
+    stroke-width: ${(p) =>
+      p.strokeWidth !== undefined ? p.strokeWidth + 'px' : undefined};
+  }
+`;
+
+export default Icon;
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { WEIGHTS } from '../../constants';
+
+const Logo = (props) => {
+  return (
+    <Link href="/">
+      <Wrapper {...props}>Sole&amp;Ankle</Wrapper>
+    </Link>
+  );
+};
+
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+`;
+
+const Wrapper = styled.h1`
+  font-size: 1.5rem;
+  font-weight: ${WEIGHTS.bold};
+`;
+
+export default Logo;
